1b875d5b8a16b9e1862eb84a897103907314989e
unknown <syeonki66@gmail.com>
2020-06-14 03:25:00 +0900

    combine led, tactsw, clcd

--- clcd_test.c
+++ clcd_test.c
@@ -11,10 +11,19 @@
 #include<string.h>
 #define clcd "/dev/clcd"
 
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include<sys/stat.h>
 
+#define led "/dev/led"
 
 static char tactswDev[] = "/dev/tactsw";
 static int  tactswFd = (-1);
+
+
+int led_count = 0;
+
+
 
 unsigned char tactsw_get(int tmo)
 {   
@@ -53,10 +62,11 @@ int tact_switch(){
 	while(1){
 		c = tactsw_get(10);
         switch (c) {
+			case 1:  selected_tact = 1 ; break;
 			case 2:  selected_tact = 2 ; break;
+			case 3:  selected_tact = 3 ; break;
+			case 4:  selected_tact = 4 ; break;
 			case 5:  selected_tact = 5 ; break;
-			case 8:  selected_tact = 8 ; break;
-			case 11:  selected_tact = 11 ; break;
 
 			default: printf("press other key\n"); break;
         }
@@ -66,23 +76,59 @@ int tact_switch(){
 }
 
 void clcd_input(char clcd_text[]){
+//	char led_count_str[10];
 	int clcd_d;
 	clcd_d = open(clcd , O_RDWR);
 
 	if (clcd_d < 0) {
 		printf("디바이스 드라이버가 없습니다.\n");
 	}
-	
+//	sprintf(led_count_str, "%d", led_count);
+//	strcat(clcd_text, led_count_str);
 	write(clcd_d , clcd_text , strlen(clcd_text)); // 1.디스크립터  2.문자들 3.문자들의 크기
 	close(clcd_d);
 }
 
 
+void led_control(){
+	int dev;
+
+	unsigned char data;
+	
+	char led_array[] = { 0xFF,0xFE,0xFC,0xF8,0xF0,0xE0,0xC0,0x80,0x00 };
+	
+	//1. 해당 드라이버 경로
+	//2. O_RDWR
+
+	dev = open(led,O_RDWR);
+
+	if(dev <0){
+		printf("열수 없습니다.\n");
+		exit(0);
+	}
+
+	led_count++;
+	data = led_array[led_count]; // binary controlled
+		
+	//1. 파일의 디스크립트
+	//2. 데이터 전달
+	//3. 크기를 넣어준다.
+
+	write(dev , &data , sizeof(unsigned char));
+	usleep(100000);
+	
+
+	close(dev);
+}
+
 int main()
 {
 	while(1){
 		if (tact_switch()){
-			clcd_input("Bet amount :");
+			clcd_input("Bet amount : ");
+			if (tact_switch() == 4){
+				led_control();
+        		}
 		}
 	}	
 		
