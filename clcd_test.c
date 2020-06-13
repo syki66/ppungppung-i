dfb12da37b509234627e22514cca070d626b5da2
unknown <syeonki66@gmail.com>
2020-06-14 00:43:29 +0900

    modulize clcd

--- clcd_test.c
+++ clcd_test.c
@@ -1,32 +1,26 @@
 #include<stdio.h>
-
 #include<stdlib.h>
-
 #include<fcntl.h>
-
 #include<string.h>
-
-
-
 #define clcd "/dev/clcd"
 
-char start_text[] = "press any key to start game";
+void clcd_input(char clcd_text[]);
 
 int main() {
+	clcd_input("press any key to start game");
+	
+	return 0;
+}
+
+
+void clcd_input(char clcd_text[]){
 	int clcd_d;
 	clcd_d = open(clcd , O_RDWR);
 
-	if (clcd_d <0) {
+	if (clcd_d < 0) {
 		printf("디바이스 드라이버가 없습니다.\n");
-		return 0;
 	}
-
-
-	//1.디스크립터  2.문자들 3.문자들의 크기
-
-	write(clcd_d , start_text , sizeof(start_text));
+	
+	write(clcd_d , clcd_text , strlen(clcd_text)); // 1.디스크립터  2.문자들 3.문자들의 크기
 	close(clcd_d);
-
-	return 0;
-
 }
